11498a466d942311b34de2de5f6d7a7180edd704
BLOOD-XMD <Bloodzme5@gmail.com>
2025-08-02 17:08:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'jinwoo:~c6dT2SDD#IO-791HT81iVizeSUnpbgqrD4azNhQ_mnXykWX2bwBg' 
 //Enter your Jinwoo session id here; must start with jinwoo:~
 
 //--------------------[ BOT NAME ]----------------------//
